ae1b539bf0255e5e01fb0f7e5ce77fe471700ebd
jlafleur93 <josephlafleur@hotmail.com>
2022-01-28 00:09:31 -0700

    finished gophercise

--- main.go
+++ main.go
@@ -5,19 +5,21 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
-var correctPoints int = 0;
+
+var correctPoints int = 0
 
 func main() {
 	csvFile := flag.String("csv", "problems.csv", "a csv file in format of ans,que")
+	timeLimit := flag.Int("limit", 30, "sets the timer for the quiz in seconds")
+
 	flag.Parse()
 	_ = csvFile
 
-	flagTimer := flag.String("timer")
-
 	file, err := os.Open(*csvFile)
 	if err != nil {
-		exitPrint(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFile ))
+		exitPrint(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFile))
 	}
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
@@ -26,26 +28,46 @@ func main() {
 	}
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+problemloop:
 	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+
+		case answer := <-answerCh:
+			if answer == p.a {
+				correctPoints++
+			}
+		}
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
 		var answer string
 		fmt.Scanf("%s \n", &answer)
-		if answer == p.a{
+		if answer == p.a {
 			fmt.Println("Correct!")
-			correctPoints++;
+			correctPoints++
 
 		}
-		if i == 11{
-		var wrongAnswers = 12 - correctPoints
-		fmt.Println("You have gotten", correctPoints, "Questions correct")
-		fmt.Println("You have gotten",wrongAnswers, "questions wrong" )
+		if i == 11 {
+			var wrongAnswers = 12 - correctPoints
+			fmt.Println("You have gotten", correctPoints, "Questions correct")
+			fmt.Println("You have gotten", wrongAnswers, "questions wrong")
+		}
 	}
-	}
-	
+
 }
-func parseLines(lines [][] string)[]problem {
+func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
-	for i, line := range lines{
+	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
 			a: line[1],
@@ -55,12 +77,13 @@ func parseLines(lines [][] string)[]problem {
 	return ret
 
 }
-type problem struct{
+
+type problem struct {
 	q string
 	a string
 }
 
-func exitPrint(msg string){
+func exitPrint(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
